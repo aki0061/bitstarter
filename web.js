a309497483da016397518a9052031ebcae89010a
A Hayashi <example@gmail.com>
2013-07-07 01:43:55 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  var indexFile = "./index.html";
+  fs.readfile(indexFile,function ( err,data){
+    if(err) throw err;
+    console.log(data);
+  });
+ 
+  //response.send('Hello World2!');
+  response.send(data.toString);
+
 });
 
 var port = process.env.PORT || 5000;
